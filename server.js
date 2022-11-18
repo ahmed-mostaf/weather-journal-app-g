07dfd09d3e6bc9b484267ddc19e8fbb1aad7d87d
ahmed-mostaf <113672935+ahmed-mostaf@users.noreply.github.com>
2022-11-18 22:27:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
