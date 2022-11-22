54d75d6e0ffe450bafc682340a3f15f775df967b
ahmed-mostaf <113672935+ahmed-mostaf@users.noreply.github.com>
2022-11-22 23:49:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
-
 // Require Express to run server and routes
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -11,11 +10,10 @@ const app = express();
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 // Cors for cross origin allowance
 app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
-
-
 // Setup Server
+app.listen(4000, () => console.log("running server on {4000}"));
+
