5cae87235821104566061cf4c13cbe93dbe6d6f1
ahmed-mostaf <113672935+ahmed-mostaf@users.noreply.github.com>
2023-01-29 23:57:35 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,5 +1,6 @@
 /* Global Variables */
 let texts = document.getElementById("feelings");
+const event_Button = document.getElementById("generate");
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
